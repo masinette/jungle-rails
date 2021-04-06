c59a147f3fee505dad2e63c94ee4bc9d47085b82
masinette <masinette.ts@gmail.com>
2021-04-05 20:11:27 -0400

    add authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,24 +5,20 @@ class SessionsController < ApplicationController
 
 
   def create
-    user = User.find_by_email(params[:email])
-    puts user.inspect
-    puts user.authenticate(params[:password])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
+    user = User.authenticate_with_credentials(params[:email], params[:password])
+    # puts user.inspect
+    if user
+      # success logic, log them in
       session[:user_id] = user.id
       redirect_to '/'
     else
-    # If user's login doesn't work, send them back to the login form.
+      # failure, render login form
+      redirect_to '/login'
+    end
+    
+    def destroy
+      session[:user_id] = nil
       redirect_to '/login'
     end
   end
-
-  def destroy
-    session[:user_id] = nil
-    redirect_to '/login'
-  end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, length: { minimum: 5, maximum: 20 }, on: :create
+  
+  validates :email, presence:true, confirmation: { case_sensitive: false }, uniqueness: true
+  
+  def self.authenticate_with_credentials( email, password )
+    user = User.find_by_email(email)
+
+    if user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
 end
